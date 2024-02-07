db83dea9718925de70e9d83dac63bf4a6ef2c6be
Suryakant Raj <135504755+Suryakant018@users.noreply.github.com>
2024-02-07 12:19:52 +0530

    Time: 17 ms (50.73%), Space: 45.5 MB (48.36%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,42 +1,24 @@
 class Solution {
-    class Pair{
-        char ch;
-        int count;
-        Pair(char ch,int count){
-            this.ch=ch;
-            this.count=count;
-        }
-    }
     public String frequencySort(String s) {
-        int[] arr=new int[125];
-        for(char ch:s.toCharArray()){
-            arr[ch]++;
-        }
-        LinkedList<Pair> freq_map=new LinkedList<>();
-        for(int i=48;i<=122;i++){
-            if(arr[i]>0){
-                freq_map.add(new Pair((char)i,arr[i]));
-            }
-        }
-        Collections.sort(freq_map,new Comparator<Pair>(){
-            public int compare(Pair a,Pair b){
-                return b.count-a.count;
-            }
 
+        Map<Character, Integer> mp = new HashMap<>();
+        for(char x : s.toCharArray()){
+           mp.put(x, mp.getOrDefault(x,0)+1);
+        }
+        List<Map.Entry<Character, Integer>> lt = new ArrayList<>(mp.entrySet());
+        Collections.sort(lt, new Comparator<Map.Entry<Character, Integer> >(){
+            public int compare(Map.Entry<Character, Integer>a , Map.Entry<Character, Integer> b){
+                return b.getValue()-a.getValue();
+            }
         });
-        StringBuilder sb=new StringBuilder();
-        for(Pair p:freq_map){
-            int count=p.count;
-
-            while(count!=0){
-                sb.append(p.ch);
-                count--;
+        StringBuffer b = new StringBuffer();
+        for(Map.Entry<Character, Integer> e : lt){
+            int i=0;
+            while(e.getValue()>i){
+                b.append(e.getKey());
+                i++;
             }
-
         }
-        return sb.toString();
-        
-
-
+        return b.toString();
     }
 }
