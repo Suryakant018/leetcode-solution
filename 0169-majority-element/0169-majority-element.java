9f83289d1192a5fda5275c20f2aa7a4c06c62b57
Suryakant Raj <135504755+Suryakant018@users.noreply.github.com>
2024-02-12 09:32:58 +0530

    Time: 4 ms (55.98%), Space: 51.3 MB (5.71%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,20 +1,7 @@
+
 class Solution {
     public int majorityElement(int[] nums) {
-        int median = nums.length / 2;
-        int ans = 0;
-
-        Map<Integer, Integer> frequency = new HashMap<>();
-
-        for (int num : nums) {
-            frequency.put(num, frequency.getOrDefault(num, 0) + 1);
-        }
-
-        for (int key : frequency.keySet()) {
-            if (frequency.get(key) > median) {
-                ans = key;
-            }
-        }
-
-        return ans;
+        Arrays.sort(nums);
+        return nums[nums.length/2];
     }
 }
